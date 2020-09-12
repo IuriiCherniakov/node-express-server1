b33a528348f77d01af2f4d62a1f8fad9e0c63b13
IuriiCherniakov <chernyakov.yuriy@gmail.com>
2020-09-11 22:40:14 -0400

    fixed logger

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import express from 'express';
 import home from "./home";
 import info from "./info";
 import errorHandler from "./errorHandler";
+import logger from "./logger";
 
 const app = express();
 const PORT = 5000;
 //localhost:5000/
 
+logger(app);
 app.get('/', home);
 app.post('/info', info);
-
-errorHandler(app)
+errorHandler(app);
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
