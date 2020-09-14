f61bc79c4cb7f98f4c8ba4f51a5d35b460707832
IuriiCherniakov <chernyakov.yuriy@gmail.com>
2020-09-13 21:02:36 -0400

    added babel

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const PORT = 5000;
 //localhost:5000/
 
-app.get('/', home )
-app.post('/info', info )
+app.get('/', home);
+app.post('/info', info);
 
-function home (req, res) {
-    res.send('HOME')
+function home(req, res) {
+    res.send('HOME--------------');
 }
-function info (req, res) {
-    const a = 123;
+function info(req, res) {
+    const a = 123; 
     const b = Math.random();
     const c = a + b;
-    res.send('INFO' + c)
+    res.send('INFO' + c);
 }
 
 
- app.listen(PORT, () => {
-     console.log(`Example app listening at http://localhost:${PORT}`)
- })
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
 
 
 
